4f9120d657a404b6585f820787089b5915986b1f
tbcey74123 <tbcey74123@gmail.com>
2014-08-23 20:28:37 +0800

    Now when the window is resized, the divs in about-me page will also resize. Moreover, when the window-size is smaller than a specific value ,the divs will all shift to the left.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,42 @@
 $(document).ready(function(){
+    SetFontSize(0.06);
+    
     $('#menu-button').click(function(){
         $('#side-bar').animate({left: "0px"},700);
     });
     $('#close-button').click(function(){
         $('#side-bar').animate({left: "-200px"},700);
     });
+
+    $(window).resize(function(){
+	if( window.innerWidth <= 680 ) {
+	    SetToOneSide();
+	}else{
+	    SetToBothSide();
+	    SetFontSize(0.06);
+        }    	
+	$('.about-me-div').css("height",Math.ceil(window.innerWidth * 0.45 * 1.25));
+    });
 });
+
+var SetToOneSide = function() {
+	$('.about-me-right').addClass("about-me-left");
+	$('.about-me-left').css("width","90%");
+	$('.about-me-middle').css("float","right");
+	$('.about-me-none').css("display","none");
+	SetFontSize(0.12);
+}
+
+var SetToBothSide = function() {
+	$('.about-me-right').removeClass("about-me-left");
+	$('.about-me-left').css("width","45%");
+	$('.about-me-right').css("width","45%");
+	$('.about-me-middle').css("float","left");
+	$('.about-me-none').css("display","inline");
+	SetFontSize(0.12);
+}
+var SetFontSize = function(num) {
+    var width = window.innerWidth;
+    var fontsize = Math.ceil(width * 0.45 * num);
+    $('.about-me-div').css("font-size",fontsize);
+}
