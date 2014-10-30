6aaf57e217f954e43d939a8fc05a7eceb109e3f8
tbcey74123 <tbcey74123@gmail.com>
2014-10-31 00:41:01 +0800

    test
    test

---
+++ js/waterfall.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$('.item img').attr("width",width);
+	$('#main-frame').imagesLoaded(function () {
+		waterfall();
+	});
+});
+
+var waterfall = function() {
+	$('#main-frame').masonry({
+		itemSelector: '.item',
+		columnWidth: width,
+		gutter: 10,
+		animate: true
+	});
+}
+
+var width = Math.round((window.innerWidth - 200) / 5 - 25);
